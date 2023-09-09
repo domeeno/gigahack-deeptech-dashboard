c9e84e7b15112a6c5c2316412fbaea78b7bb9756
domeeno <dominicflocea.dev@gmail.com>
2023-09-10 01:05:50 +0300

    init backend

---
+++ dashboard/src/test/kotlin/com/gigahack/dashboard/DashboardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gigahack.dashboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DashboardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
