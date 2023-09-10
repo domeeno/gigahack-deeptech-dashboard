ebf0836991933e1681bdd94d086d706ba7972a1c
domeeno <dominicflocea.dev@gmail.com>
2023-09-10 13:42:12 +0300

    add spring python interpreter

--- dashboard/src/main/kotlin/com/gigahack/dashboard/controller/PredictController.kt
+++ dashboard/src/main/kotlin/com/gigahack/dashboard/controller/PredictController.kt
@@ -1,4 +1,28 @@
 package com.gigahack.dashboard.controller
 
-class PredictController {
+import org.python.core.PyObject
+import org.python.util.PythonInterpreter
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+
+@RequestMapping("/predict")
+@RestController
+class PredictController(
+  private val interpreter: PythonInterpreter
+) {
+
+  @PostMapping()
+  fun makePredict(@RequestParam days: Int): String {
+    interpreter.exec("future = m.make_future_dataframe(periods=$days)")
+    interpreter.exec("forecast = m.predict(future)")
+
+    val forecast = interpreter.get("forecast")
+    val stringForecast = forecast.asString()
+    val fore = forecast.toString()
+
+    return forecast.toString()
+  }
 }
